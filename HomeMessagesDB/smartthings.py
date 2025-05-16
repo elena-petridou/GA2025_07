5ddc3c2bf4a48f68ed7f4628b52c44295c0b6105
Bassias <eba.gulzar@gmail.com>
2025-05-16 17:13:48 +0200

    added querying option

--- HomeMessagesDB/smartthings.py
+++ HomeMessagesDB/smartthings.py
@@ -3,16 +3,48 @@
 #or in general smartthings.py -d dburl *tsv
 #the code will not pick anything up as the files presented in the assig are tsv.gz
 #should we build in a save net for that?
+from datetime import datetime
 import click
 import glob
 import os
 
 @click.command() #function below should be treated as cli command
-@click.option('-d','--dburl','d', help='flavor text as of now') # option -d for user
+@click.option('-d','--dburl','d', help='flavor text as of now', required = True) # option -d for user
+@click.option('--query', default=None, is_flag=True, help='Run a query instead of inserting files')
 @click.argument('files',nargs=-1)
 
-def present_files(d,files):
+def main(d,files,query):
     """Very clear explanation of the function which will be written in the future"""
+    
+    from home_messages_db import HomeMessagesDB
+    db = HomeMessagesDB(d)
+    db.create_db()
+    
+    if query:
+        click.echo('Would you like output specified on date, object or size of the database?')
+        userinp= input()
+        if userinp.lower() =='date':
+            click.echo('from when until when? YYYY-mm-dd:YYYY-mm-dd')
+            timeinp=input()
+            if ':' in timeinp:
+                dates=timeinp.split(':')
+                datepars1=list(map(int, dates[0].split('-')))
+                date1= int(datetime(*datepars1).timestamp())
+                
+                datepars2=list(map(int, dates[1].split('-')))
+                date2=int(datetime(*datepars2).timestamp())
+            else:
+                datepars1=list(map(int, timeinp.split('-')))
+                date1= int(datetime(*datepars1).timestamp())
+                date2=date1
+
+            output=db.query_db(f'SELECT * FROM smartthings WHERE epoch >= {1665346388} AND epoch <={1665346402}')
+            click.echo(output)
+        elif userinp=='all':
+            output=db.query_db(f'SELECT * FROM smartthings')
+            click.echo(output)
+        return
+    
     all_files=[] #empty list to gather files from users input
     #loop over everything in files, user specified
     for pattern in files:
@@ -29,6 +61,7 @@ def present_files(d,files):
             ##although its cli so ???
             if len(matched_files)==0:
                 click.echo('No files found using pattern!')
+                return None
         
         #in the case the user wrote down filepaths without wildcards        
         else:    
@@ -38,15 +71,27 @@ def present_files(d,files):
                 all_files.append(pattern)
             else:
                 click.echo(f'No file "{pattern}" found!')
-                
-    #add to database
-    from home_messages_db import HomeMessagesDB
+                return None
 
-    db = HomeMessagesDB(d)
-    db.create_db()
-    with click.progressbar(all_files, label='inserting files', length=files) as bar:
-        for f in bar:
-            db.insert_table_smartthings(f)
-            
+    with click.progressbar(all_files, label='inserting files') as bar:
+        for files in bar:
+            db.insert_table_smartthings(files)
 
+## this does not work as -d gets the dburl, so add a second option to dburL?
+## No def make a new command for this  
     
+
+if __name__ == '__main__':
+    main()
+    
+
+
+int(datetime(*[2021,7,26]).timestamp())
+
+
+timeinp=input()
+print(timeinp)
+datepars1=list(map(int, timeinp.split('-')))
+print(datepars1)
+date1= int(datetime(*datepars1).timestamp())
+print(date1)
